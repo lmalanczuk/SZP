118a672d37b9588088307be6da4928d0f806fe17
Łukasz Małańczuk <l.malanczuk03@gmail.com>
2024-05-12 22:45:03 +0200

    teoretycznie działa ale jednak nie do konca (naprawic post użytkownika na postmanie pozdro)

--- src/main/java/com/example/SZP/registration/RegistrationController.java
+++ src/main/java/com/example/SZP/registration/RegistrationController.java
@@ -1,6 +1,8 @@
 package com.example.SZP.registration;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "api/v1/registration")
 @AllArgsConstructor
+@NoArgsConstructor
 public class RegistrationController {
 
     private RegistrationService registrationService;
 
+    @PostMapping
     public String register(@RequestBody RegistrationRequest request) {
         return registrationService.register(request);
     }
--- src/main/java/com/example/SZP/registration/RegistrationService.java
+++ src/main/java/com/example/SZP/registration/RegistrationService.java
@@ -1,10 +1,33 @@
 package com.example.SZP.registration;
 
+import com.example.SZP.appuser.AppUser;
+import com.example.SZP.appuser.AppUserRole;
+import com.example.SZP.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
+
+
     public String register(RegistrationRequest request) {
-        return "works";
+        boolean isValidEmail = emailValidator.
+                test(request.getEmail());
+        if (!isValidEmail) {
+            throw new IllegalStateException("email not valid");
+        }
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+                )
+        );
     }
 }
