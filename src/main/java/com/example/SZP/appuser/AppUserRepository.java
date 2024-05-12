a0963621b9280663b9c257fbfb478fc82912b439
Łukasz Małańczuk <l.malanczuk03@gmail.com>
2024-05-12 21:33:16 +0200

    nie wiem co sie sdzieje

--- src/main/java/com/example/SZP/appuser/AppUserRepository.java
+++ src/main/java/com/example/SZP/appuser/AppUserRepository.java
@@ -1,11 +1,12 @@
 package com.example.SZP.appuser;
 
 import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 @Repository
 @Transactional
-public interface AppUserRepository {
+public interface AppUserRepository extends JpaRepository<AppUser, Long>{
     Optional<AppUser> findByEmail(String email);
 }
